4aa861778c1f53fa79696ff98d77bb536ba7c4de
lovepreet <klovepreet12199@gmail.com>
2022-07-13 18:26:25 -0400

    Initial Commit

---
+++ Calculator (My First App)/Calculator__My_First_App_App.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator__My_First_App_App.swift
+//  Calculator (My First App)
+//
+//  Created by Macbook on 2022-07-13.
+//
+
+import SwiftUI
+
+@main
+struct Calculator__My_First_App_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
